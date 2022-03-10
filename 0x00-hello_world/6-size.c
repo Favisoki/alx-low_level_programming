2173782a59fca6e9cc3f0714c37a361077bd6a07
Favisoki <adedokunisaac783@gmail.com>
2022-03-10 15:50:25 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints quotes
+ *
+ * Return: Zero on success
+ *
+ */
+int main(void)
+{
+	int charSize = sizeof(char);
+	int intSize = sizeof(int);
+	int longSize = sizeof(long int);
+	int longlongSize = sizeof(lomg long int);
+	int floatsize = sizeof(float);
+	printf("Size of a character: %d byte(s)\n", n);
+	printf
+	return (0);
+}
