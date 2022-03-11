4ca30140a4d7b317c89419783a2f38a4803b537a
Favisoki <adedokunisaac783@gmail.com>
2022-03-11 10:32:22 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <time.h>
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: zero
+ *
+ */
+
+int main(void)
+{
+	char c;
+
+
+	for (c = 'a'; c <= 'z'; ++c)
+	{
+		putchar(c);
+		for (C = 'A'; C <= 'Z'; ++C)
+		{
+			putchar(C);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
