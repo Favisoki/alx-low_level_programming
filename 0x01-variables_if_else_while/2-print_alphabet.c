df553b36bd46b0715c295420f50ea2486796b71e
Favisoki <adedokunisaac783@gmail.com>
2022-03-11 09:04:27 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <time.h>
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: zero
+ *
+ */
+
+int main(void)
+{
+	char c;
+
+
+	for (c = 'a'; c <= 'z'; ++c)
+	{
+		putchar(c);
+	}
+
+	return (0);
+}
