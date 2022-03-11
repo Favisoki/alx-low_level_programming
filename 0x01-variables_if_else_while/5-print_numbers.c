26d6e707185e9f516d03f481c38302ffaf73db77
Favisoki <adedokunisaac783@gmail.com>
2022-03-11 10:49:40 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <time.h>
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: zero
+ *
+ */
+
+int main(void)
+{
+	int n;
+
+
+	for (n = 0; n < 10; n++)
+	{
+		printf(n);
+	}
+	printf("\n");
+
+	return (0);
+}
