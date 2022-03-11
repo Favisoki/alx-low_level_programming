ab514bd805eaf81184e774a05f11ac09c5d78565
Favisoki <adedokunisaac783@gmail.com>
2022-03-11 10:42:23 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <time.h>
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: zero
+ *
+ */
+
+int main(void)
+{
+	char c;
+
+
+	for (c = 'a'; c <= 'z'; ++c)
+	{
+		if (c == q || c == e)
+		{
+			/*Do nothing*/
+		}
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
