7a46d96c613cb315ab036ec11d37033f21b2dbcf
Favisoki <adedokunisaac783@gmail.com>
2022-03-11 10:57:16 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <time.h>
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: zero
+ *
+ */
+
+int main(void)
+{
+	int n;
+
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n);
+	}
+	putchar("\n');
+
+	return (0);
+}
