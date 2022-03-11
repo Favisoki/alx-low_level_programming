304abb92b1f9a25d882df5cccd63a68ee6c1c10b
Favisoki <adedokunisaac783@gmail.com>
2022-03-11 06:19:47 -0800

    Fix 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,11 @@ int main(void)
 	int longSize = sizeof(long int);
 	int longlongSize = sizeof(lomg long int);
 	int floatsize = sizeof(float);
-	printf("Size of a character: %d byte(s)\n", n);
-	printf
+
+	printf("Size of a character: %d byte(s)\n", charSize);
+	printf("Size of an int: %d byte(s)\n", intSize);
+	printf("Size of a long: %d byte(s)\n", longSize);
+	printf("Size of a long long int: %d byte(s)\n", longlongSize);
+	printf("Size of a float: %d byte(s)\n", floatSize);
 	return (0);
 }
