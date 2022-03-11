e8940328d8060921560e9c84aa9fc1224790ec01
Favisoki <adedokunisaac783@gmail.com>
2022-03-11 06:24:13 -0800

    Fix taks 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,7 +16,7 @@ int main(void)
 
 	printf("Size of a character: %d byte(s)\n", charSize);
 	printf("Size of an int: %d byte(s)\n", intSize);
-	printf("Size of a long: %d byte(s)\n", longSize);
+	printf("Size of a long int: %d byte(s)\n", longSize);
 	printf("Size of a long long int: %d byte(s)\n", longlongSize);
 	printf("Size of a float: %d byte(s)\n", floatSize);
 	return (0);
