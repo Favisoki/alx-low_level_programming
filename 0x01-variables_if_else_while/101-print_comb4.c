de7a0eb9b7bb0adba10557ffa3ff1e30725766e5
Favisoki <adedokunisaac783@gmail.com>
2022-03-12 14:55:53 -0800

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <time.h>
+#include <stdio.h>
+
+
+/**
+ * main - prints 00 - 99
+ *
+ * Return: 0 for success
+ *
+ */
+
+int main(void)
+{
+	int n, o, p;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		for (o = '0'; o <= '9'; o++)
+		{
+			for (p = '0'; p <= '9'; p++)
+			{
+				if ((n < o) & (o < p) & (p <= '9'))
+				{
+					putchar(n);
+					putchar(o);
+					putchar(p);
+					if ((o + n + p) < '168')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
