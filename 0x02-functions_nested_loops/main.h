d31373955042920dccaebedd3b87e7b9c595b752
Favisoki <adedokunisaac783@gmail.com>
2022-03-16 07:55:51 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,6 +3,15 @@
 /**
  * _putchar - writes the character c to stdout
  *
+ * print_alphabet - prints a to z
+ *
+ * print_alphabet_x10 - prints a to x 10 times
+ *
+ * _islower - checks is a character is lowercase
  */
 
+
 int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
