dc630b3c4b59a4224906552dd8b2c542d81234c2
Favisoki <adedokunisaac783@gmail.com>
2022-03-18 15:49:32 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -22,6 +22,8 @@
  * times_table - prints 9 times table
  *
  * add -adds two integers
+ *
+ * print_to_98 - prints all numbers from n to 98
  */
 
 
@@ -36,3 +38,4 @@ void jack_bauer(void);
 void times_table(void);
 int print_last_digit(int);
 int add(int, int);
+void print_to_98(int);
