9924bc33a0ef51315075b4d5c0eabafffdd9ef21
Favisoki <adedokunisaac783@gmail.com>
2022-03-18 13:16:16 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *mul - multiplies two numbers
+ *@a: first number
+ *@b: second number
+ *
+ * Return: 1 if uppercase, 0 if lowercase
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
