ea03bc4edcfe16a5e45c3a905e4262731a5e78e0
Favisoki <adedokunisaac783@gmail.com>
2022-03-18 13:17:54 -0700

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,7 +5,10 @@
  *_isupper - checks if a character is uppercase
  *
  * _isdigit - checks if a character is a digit
+ *
+ * mul - multiplies two digits
  */
 
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
