d255e148dd4974ec41f7ab3fef20e7a0607ea92d
Favisoki <adedokunisaac783@gmail.com>
2022-03-18 05:53:37 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,16 +8,19 @@
 */
 int print_sign(int n)
 {
-	if (n > '0')
+	if (n > 0)
 	{
-		putchar(n);
+		putchar('+');
 		return (1);
 	}
-	else if (n == '0')
+	else if (n == 0)
 	{
-		putchar(n);
+		putchar('0');
 		return (0);
 	}
 	else
+	{
+		putchar('-');
 		return (-1);
+	}
 }
