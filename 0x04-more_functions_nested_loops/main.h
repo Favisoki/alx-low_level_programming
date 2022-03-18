d238e73410833ef517865c0a10c2ec2f355339e2
Favisoki <adedokunisaac783@gmail.com>
2022-03-18 16:47:25 -0700

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -9,9 +9,12 @@
  * mul - multiplies two digits
  *
  * print_numbers - prints all numbers from 0 to 9
+ *
+ * print_most_numbers - prints all numbers from 0 to 9 except 2 and 4
  */
 
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
+void print_most_numbers(void);
