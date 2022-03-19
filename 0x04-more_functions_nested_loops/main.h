2a2d3362a7bfff9c1de878ef6580c6101cdd1e42
Favisoki <adedokunisaac783@gmail.com>
2022-03-18 17:15:07 -0700

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,6 +7,8 @@
  * mul - multiplies two digits
  * print_numbers - prints numbers 0 to 9
  * print_most_numbers - prints numbers 0 to 9 excpt 2/4
+ * more_numbers - print 0-14, 10 times
+ *print_line - draw straight line
  *
  */
 
@@ -15,3 +17,5 @@ int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
