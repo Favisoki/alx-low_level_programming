a6240b4a2fec85bc8835ca60e2d0163444d76211
Favisoki <adedokunisaac783@gmail.com>
2022-03-18 17:53:32 -0700

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,6 @@
 #include <unistd.h>
 
 /**
- *
  *_isupper - checks if a character is uppercase
  * _isdigit - checks if a character is a digit
  * mul - multiplies two digits
@@ -9,6 +8,7 @@
  * print_most_numbers - prints numbers 0 to 9 excpt 2/4
  * more_numbers - print 0-14, 10 times
  *print_line - draw straight line
+ print_diagonal - print diagonal line 
  *
  */
 
@@ -19,3 +19,4 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
+void print_diagonal(int n);
