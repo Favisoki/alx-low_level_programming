33c97c2c00b3fb969b339baa347f6ff83f7014a9
Favisoki <adedokunisaac783@gmail.com>
2022-03-18 18:02:49 -0700

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #include <unistd.h>
 
 /**
+ * _putchar - writes the character c to stdout
  *_isupper - checks if a character is uppercase
  * _isdigit - checks if a character is a digit
  * mul - multiplies two digits
@@ -12,6 +13,7 @@
  *
  */
 
+int _putchar(char);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
