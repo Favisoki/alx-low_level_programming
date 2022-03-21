2969fb06a262af8ef63bf1b6a4751e11b4c63eff
Favisoki <adedokunisaac783@gmail.com>
2022-03-21 15:41:57 -0700

    INitial commit lesson 5

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to 98 - take a pointer to an int and update the value pointed to, to 98
+ *
+ * Return: nothing
+ */ 
+
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#include <unistd.h>
+
+/**
+ * reset_to_98 - take a pointer to an int and update the value pointed to, to 98
+ *
+ */
+
+void reset_to_98(int *n);
