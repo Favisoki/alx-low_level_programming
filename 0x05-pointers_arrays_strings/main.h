6f8c54c6c8958646776a24880cb7a935d16e2f3e
Favisoki <adedokunisaac783@gmail.com>
2022-03-21 16:10:24 -0700

    main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,7 +3,9 @@
 /**
  * reset_to_98 - take a pointer to an int and update the value pointed to, to 98
  * swap_int - swap the values of two integers
+ * _strlen - return the length of a string
  */
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
