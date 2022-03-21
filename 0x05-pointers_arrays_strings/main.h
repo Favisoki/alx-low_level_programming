0a98ec64774ae1f29eec2a97f82ada02ed3ea00a
Favisoki <adedokunisaac783@gmail.com>
2022-03-21 16:57:03 -0700

    main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,10 +5,11 @@
  * swap_int - swap the values of two integers
  * _strlen - return the length of a string
  * _puts - prints a string to stdout
- *
+ * print_rev - prints a string in reverse
  */
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
+void print_rev(char *s);
