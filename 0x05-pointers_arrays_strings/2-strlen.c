1167df571617f494f21f015c03cbe61db37ff910
Favisoki <adedokunisaac783@gmail.com>
2022-03-21 16:09:30 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ *_strlen - return the length of a string
+ *@s: string to find its length
+ *
+ * Return: length of a string
+ */
+
+
+int _strlen(char *s)
+{
+	return (strlen(*s));
+}
