9f335bc8c569e07457e2c9ccc2b4a42b35e3e697
Favisoki <adedokunisaac783@gmail.com>
2022-03-21 17:43:30 -0700

    necessary fix

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ *puts2 - prints every other character of a string
+ *@str: string to use
+ *
+ * Return: nothing
+ */
+
+
+void puts2(char *str)
+{
+	int i = 0;
+	int j = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	while (j < i)
+	{
+		_putchar(s[i]);
+		j += 2;
+	}
+
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,6 +7,7 @@
  * _puts - prints a string to stdout
  * print_rev - prints a string in reverse
  * rev_string - reverses a string
+ * puts2- prints every other character of a string
  */
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -15,3 +16,4 @@ int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
+void puts2(char *str);
