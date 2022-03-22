3243e76d94d8f6e0cf9445dc36a2ea7e9330f8ab
Favisoki <adedokunisaac783@gmail.com>
2022-03-21 17:49:51 -0700

    main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,6 +8,8 @@
  * print_rev - prints a string in reverse
  * rev_string - reverses a string
  * puts2- prints every other character of a string
+ *puts_half - prints half of a string
+ *
  */
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -17,3 +19,4 @@ void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
+void puts_half(char *str);
